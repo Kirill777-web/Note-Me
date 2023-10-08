0d88ae63be1e61d922c318b326f0121100eea784
Kirill777-web <ki.lazutin@gmail.com>
2023-10-08 08:50:56 -0300

    Fixed path module reference error

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
